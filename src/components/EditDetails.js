55a8ea4c21c0cb2c43c588b70487f8e8ac4fee67
Thanh Le <nhatthanhh1993@gmail.com>
2019-09-02 21:37:32 +0300

    Navbar buttons with MyButton component

--- src/components/EditDetails.js
+++ src/components/EditDetails.js
@@ -9,7 +9,6 @@ import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogTitle from '@material-ui/core/DialogTitle';
-import IconButton from '@material-ui/core/IconButton'
 
 //Icon
 import EditIcon from '@material-ui/icons/Edit'
@@ -17,7 +16,7 @@ import EditIcon from '@material-ui/icons/Edit'
 //Redux
 import { connect } from 'react-redux'
 import { editUserDetails } from '../redux/actions/userActions'
-import { Tooltip } from '@material-ui/core';
+import MyButton from '../util/MyButton';
 
 const styles = (theme) => ({
     ...theme,
@@ -76,11 +75,9 @@ export class EditDetails extends Component {
         const { classes } = this.props
         return (
             <Fragment>
-                <Tooltip title="Edit details" placement="top">
-                    <IconButton onClick={this.handleOpen} className={classes.button}>
-                        <EditIcon color="primary" />
-                    </IconButton>
-                </Tooltip>
+                <MyButton tip="Edit Details" onClick={this.handleOpen} btnClassName={classes.button}>
+                    <EditIcon color="primary" />
+                </MyButton>
                 <Dialog open={this.state.open} onClose={this.handleClose} fullWidth maxWidth="sm">
                     <DialogTitle>Edit your details</DialogTitle>
                     <DialogContent>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,29 +1,62 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 import { Link } from "react-router-dom";
+import { connect } from 'react-redux'
+import MyButton from '../util/MyButton'
 
 //Material-UI
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Button from "@material-ui/core/Button";
 
+//Icons
+import AddIcon from "@material-ui/icons/Add"
+import HomeIcon from "@material-ui/icons/Home"
+import Notifications from "@material-ui/icons/Notifications"
+
 class Navbar extends Component {
-  render() {
-    return (
-      <AppBar>
-        <Toolbar className="nav-container">
-          <Button color="inherit" component={Link} to="/">
-            Home
-          </Button>
-          <Button color="inherit" component={Link} to="/login">
-            LogIn
-          </Button>
-          <Button color="inherit" component={Link} to="/signup">
-            SignUp
-          </Button>
-        </Toolbar>
-      </AppBar>
-    );
-  }
+    render() {
+        const { authenticated } = this.props
+        return (
+            <AppBar>
+                <Toolbar className="nav-container">
+                    {authenticated ? (
+                        <Fragment>
+                            <MyButton tip="Post a Scream!">
+                                <AddIcon />
+                            </MyButton>
+                            <Link to="/">
+                                <MyButton tip="Home">
+                                    <HomeIcon />
+                                </MyButton>
+                            </Link>
+                            <MyButton tip="Notifications">
+                                <Notifications />
+                            </MyButton>
+                        </Fragment>
+                    ) : (
+                            <Fragment>
+                                <Button color="inherit" component={Link} to="/">
+                                    Home
+                        </Button>
+                                <Button color="inherit" component={Link} to="/login">
+                                    LogIn
+                        </Button>
+                                <Button color="inherit" component={Link} to="/signup">
+                                    SignUp
+                        </Button>
+                            </Fragment>)}
+
+                </Toolbar>
+            </AppBar >
+        );
+    }
 }
 
-export default Navbar;
+const mapStateToProps = (state) => {
+    return {
+        authenticated: state.user.authenticated
+    }
+}
+
+
+export default connect(mapStateToProps)(Navbar);
