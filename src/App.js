bd7ce9238071d9de523116edcf0b6c5c3c58c396
Thanh Le <nhatthanhh1993@gmail.com>
2020-01-12 23:32:26 +0200

    fix bugs

--- src/App.js
+++ src/App.js
@@ -26,6 +26,8 @@ import axios from 'axios'
 
 const theme = createMuiTheme(themeObject);
 
+axios.defaults.baseURL = "https://us-central1-socialmediaapp-5ccbf.cloudfunctions.net/api"
+
 const token = localStorage.FBIdToken;
 if (token) {
     const decodedToken = jwtDecode(token)
